f1174a39ffa8e357667701bd1e896da5b2c9e361
SteveArms <acunoc@unsa.edu.pe>
2024-05-28 15:22:01 -0500

    Vista para la creaciond de un nuevo curso y listar estos

--- colegio/views.py
+++ colegio/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import AlumnoForm
-from .models import Alumno
+from .forms import AlumnoForm, CursoForm
+from .models import Alumno, Curso
 # Create your views here.
 def crear_alumno(request):
     if request.method == 'POST':
@@ -10,8 +10,22 @@ def crear_alumno(request):
             return redirect('lista_alumnos')
     else:
         form = AlumnoForm()
-    return render(request, 'colegio/crear_alumno.html', {'form':form})
+    return render(request, 'colegio/crear_alumno.html', {'form': form})
 
 def lista_alumnos(request):
     alumnos = Alumno.objects.all()
     return render(request, 'colegio/lista_alumnos.html', {'alumnos': alumnos})
+
+def crear_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_cursos')
+    else:
+        form = CursoForm()
+    return render(request, 'colegio/crear_curso.html', {'form':form})
+
+def lista_cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'colegio/lista_cursos.html', {'cursos': cursos})
