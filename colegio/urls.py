b48e71a4d34707f4c402a4a29b158d54ccd97722
SteveArms <acunoc@unsa.edu.pe>
2024-05-28 16:52:11 -0500

    Agregando las urls

--- colegio/urls.py
+++ colegio/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('listaAlumno/', views.lista_alumnos, name='lista_alumnos'),
     path('crearCurso/', views.crear_curso, name='crear_curso'),
     path('listaCurso/', views.lista_cursos, name='lista_cursos') ,
+    path('crearNota/', views.crear_nota, name='crear_nota'),
+    path('listaNota/', views.lista_notas, name='lista_notas'),
 ]
