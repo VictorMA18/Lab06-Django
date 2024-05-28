0bf96c79bfc452807e386356774b6a50f73ad951
SteveArms <acunoc@unsa.edu.pe>
2024-05-28 10:13:24 -0500

    Implementando la clase Nota como Modelo

--- colegio/models.py
+++ colegio/models.py
@@ -10,3 +10,8 @@ class Curso(models.Model):
     curso = models.CharField(max_length=100, unique=True)  
     def __str__(self):
         return self.curso
+
+class Nota(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=4, decimal_places=2, default=0)
