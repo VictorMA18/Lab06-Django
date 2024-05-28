e241f5f23073aaeddd5ea25e077d038a020ac370
SteveArms <acunoc@unsa.edu.pe>
2024-05-28 15:22:23 -0500

    Agregando las urls correspondientes

--- colegio/urls.py
+++ colegio/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('crear/', views.crear_alumno, name='crear_alumno'), 
-    path('listar/', views.lista_alumnos, name='lista_alumnos'),  
+    path('crearAlumno/', views.crear_alumno, name='crear_alumno'), 
+    path('listaAlumno/', views.lista_alumnos, name='lista_alumnos'),
+    path('crearCurso/', views.crear_curso, name='crear_curso'),
+    path('listaCurso/', views.lista_cursos, name='lista_cursos') ,
 ]
