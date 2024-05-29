b3d8a2b8f0b4d938a62eb1532494b62ade7f6c58
SteveArms <acunoc@unsa.edu.pe>
2024-05-28 23:26:36 -0500

    Registramos los modelos en el sitio de Administracion para la gestion de datos

--- colegio/admin.py
+++ colegio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Curso, Nota, Alumno
 # Register your models here.
+admin.site.register(Curso)
+admin.site.register(Nota)
+admin.site.register(Alumno)
